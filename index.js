8ee80ca73b74c399ad53d574b0c538f4fc389841
spiiralol <henderson.finlay.0@icloud.com>
2021-03-14 12:55:41 +0000

    init commit

--- index.js
+++ index.js
@@ -3,5 +3,32 @@ const token = process.env.TOKEN
 const { prefix, mongooseString } = require('./config.json')
 const fs = require('fs')
 const mongoose = require('mongoose')
-mongoose.connect
-const { Client } = require('discord.js')
+mongoose.connect(mongooseString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+
+const db = new mongoose.model('modmail-transcript', 
+    new mongoose.Schema({
+        AuthorID: String,
+        Content: Array
+    })
+)
+const { Client, Message, MessageEmbed, MessageAttachment } = require('discord.js')
+const bot = new Client()
+
+bot.on('ready', () => {
+    console.log(`${bot.user.username} has logged in.`)
+    client.user.setActivity(`your DM's`, { type: 'WATCHING' })
+      .catch(console.error);   
+})
+
+/**
+ * @param {Message} message
+ * @param {Client} bot
+ */
+
+bot.on('message', async(message) => {
+    if (message.author.bot) return;
+    const args = message
+})
